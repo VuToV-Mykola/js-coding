9bec594033a4e5aa918d2f4d39b12fc1472afa67
VuToV <vutov_nikola@icloud.com>
2021-05-18 21:58:20 +0300

    addToVSCodeTurboconsoleLog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 console.log('JavaScript is awesome'.length);
-console.log('JavaScript is awesome'.toUpperCase())
-console.log("HELLO")
+console.log('JavaScript is awesome'.toUpperCase());
+console.log('HELLO');
+сonsole.log();
+const l = 25;
+console.log('🚀 ~ file: script.js ~ line 6 ~ l', l);
