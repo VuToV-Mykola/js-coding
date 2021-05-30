5a02ab28ac70c8fbf69e4d3a2cd15ae247081eff
VuToV <vutov_nikola@icloud.com>
2021-05-30 23:40:30 +0300

    addMassiveTask-05

--- js/calculator.js
+++ js/calculator.js
@@ -130,3 +130,9 @@ function checkStorage(available, ordered) {
 }
 
 const fruits = ['apple', 'plum', 'pear', 'orange'];
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// Change code below this line
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[fruits.length - 1];
