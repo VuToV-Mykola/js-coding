8bf2ce22043df9f950a87eb0c3d1e842291c0b39
VuToV <vutov_nikola@icloud.com>
2021-05-31 22:10:39 +0300

    addMassiveTask-05

--- js/calculator.js
+++ js/calculator.js
@@ -136,3 +136,15 @@ const fruits = ['apple', 'plum', 'pear', 'orange'];
 const firstElement = fruits[0];
 const secondElement = fruits[1];
 const lastElement = fruits[fruits.length - 1];
+
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// Write your code under this line
+fruits[1] = 'peach';
+fruits[3] = 'banana';
+//Task-06
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// Write your code under this line
+fruits[1] = 'peach';
+fruits[3] = 'banana';
